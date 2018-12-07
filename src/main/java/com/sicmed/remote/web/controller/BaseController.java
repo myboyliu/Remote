89cb58a61ee0c4c5c5673c33be9e568a35b32f62
MR-L0927 <37697912+MR-L0927@users.noreply.github.com>
2018-12-07 14:04:56 +0800

    创建抽象类BaseController
    创建Controller基类,基类中添加基本返回状态

--- src/main/java/com/sicmed/remote/web/controller/BaseController.java
+++ src/main/java/com/sicmed/remote/web/controller/BaseController.java
@@ -17,6 +17,8 @@ import java.util.Map;
  */
 public abstract class BaseController {
 
+    private Map<String, Object> responseMap;
+
     /**
      * 获取token
      *
@@ -39,6 +41,144 @@ public abstract class BaseController {
         CustomDateEditor ce = new CustomDateEditor(sdf, true);
         sb.registerCustomEditor(Date.class, ce);
     }
-    
 
+    /**
+     * 请求操作成功
+     *
+     * @param result
+     * @return responseMap
+     */
+    public Map SucceedRequest(Object result) {
+        responseMap = new LinkedHashMap<>();
+        responseMap.put("code", "20000");
+        responseMap.put("msg", "Request_Succeed");
+        responseMap.put("result", result);
+        return responseMap;
+    }
+
+    /**
+     * 参数错误致请求操作失败
+     *
+     * @param result
+     * @return responseMap
+     */
+    public Map BadRequestOfArguments(Object result) {
+        responseMap = new LinkedHashMap<>();
+        responseMap.put("code", "40000");
+        responseMap.put("msg", "Request_Bad_Of_Arguments");
+        responseMap.put("result", result);
+        return responseMap;
+    }
+
+    /**
+     * 添加数据请求操作失败
+     *
+     * @param result
+     * @return responseMap
+     */
+    public Map BadRequestOfInsert(Object result) {
+        responseMap = new LinkedHashMap<>();
+        responseMap.put("code", "41000");
+        responseMap.put("msg", "Request_Bad_Of_Insert");
+        responseMap.put("result", result);
+        return responseMap;
+    }
+
+    /**
+     * 添加数据请求操作成功
+     *
+     * @param result
+     * @return responseMap
+     */
+    public Map SucceedRequestOfInsert(Object result) {
+        responseMap = new LinkedHashMap<>();
+        responseMap.put("code", "41001");
+        responseMap.put("msg", "Request_Succeed_Of_Insert");
+        responseMap.put("result", result);
+        return responseMap;
+    }
+
+    /**
+     * 更新数据请求操作失败
+     *
+     * @param result
+     * @return responseMap
+     */
+    public Map BadRequestOfUpdate(Object result) {
+        responseMap = new LinkedHashMap<>();
+        responseMap.put("code", "41002");
+        responseMap.put("msg", "Request_Bad_Of_Update");
+        responseMap.put("result", result);
+        return responseMap;
+    }
+
+    /**
+     * 更新数据请求操作成功
+     *
+     * @param result
+     * @return responseMap
+     */
+    public Map SucceedRequestOfUpdate(Object result) {
+        responseMap = new LinkedHashMap<>();
+        responseMap.put("code", "41003");
+        responseMap.put("msg", "Request_Succeed_Of_Update");
+        responseMap.put("result", result);
+        return responseMap;
+    }
+
+    /**
+     * 删除数据请求操作失败
+     *
+     * @param result
+     * @return responseMap
+     */
+    public Map BadRequestOfDelete(Object result) {
+        responseMap = new LinkedHashMap<>();
+        responseMap.put("code", "41004");
+        responseMap.put("msg", "Request_Bad_Of_Delete");
+        responseMap.put("result", result);
+        return responseMap;
+    }
+
+    /**
+     * 删除数据请求操作成功
+     *
+     * @param result
+     * @return responseMap
+     */
+    public Map SucceedRequestOfDelete(Object result) {
+        responseMap = new LinkedHashMap<>();
+        responseMap.put("code", "41005");
+        responseMap.put("msg", "Request_Bad_Of_Delete");
+        responseMap.put("result", result);
+        return responseMap;
+    }
+
+    /**
+     * 查找数据请求操作失败
+     *
+     * @param result
+     * @return responseMap
+     */
+    public Map BadRequestOfSelect(Object result) {
+        responseMap = new LinkedHashMap<>();
+        responseMap.put("code", "41006");
+        responseMap.put("msg", "Request_Bad_Of_Select");
+        responseMap.put("result", result);
+        return responseMap;
+    }
+
+    /**
+     * 查找数据请求操作成功
+     *
+     * @param result
+     * @return responseMap
+     */
+    public Map SucceedRequestOfSelect(Object result) {
+        responseMap = new LinkedHashMap<>();
+        responseMap.put("code", "41007");
+        responseMap.put("msg", "Request_Bad_Of_Select");
+        responseMap.put("result", result);
+        return responseMap;
+    }
 }
