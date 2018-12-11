699466eaa2bbc439426c1ca86fbe340a83b8bc8b
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2018-12-11 18:49:26 +0800

    添加接口路径

--- src/main/resources/public/common/js/url.js
+++ src/main/resources/public/common/js/url.js
@@ -1,9 +1,20 @@
-const baseUrl = "http://127.0.0.1";
-const getAllHospital = "/hospital/select";
-const getAllBranch = "/branch/getAll";
-// const getAllBranch = "/caseContentType/selectAll";
-const getAllCaseContentType = "/casecontenttype/selectall";
-const registrationUrl = "/user/registration";
-const loginUrl = "/user/login";
+
+const baseUrl = "http://127.0.0.1"; //服务地址
+
+const getAllHospital = "/hospital/select"; //查询所有医院类别
+
+const getAllBranch = "/branch/getAll"; //查询所有科室列表
+
+const getAllCaseContentType = "/casecontenttype/selectall"; //查询所有病历类型列表
+
+const registrationUrl = "/user/registration"; //注册接口
+
+const loginUrl = "/user/login"; //登陆接口
+
+const checkPhoneNumber = "/user/login"; //校验注册手机号是否可用
 
 const getSpecialistTypeByHospitalId = "/specialistType/findByParam";
+
+const uploadFileUrl = "/file/upload";
+
+
