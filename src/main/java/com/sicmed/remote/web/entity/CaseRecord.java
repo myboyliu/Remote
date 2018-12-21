06a9c160c29e2a70daeacd2128b5c3a681a4f22b
MR-L0927 <37697912+MR-L0927@users.noreply.github.com>
2018-12-21 15:46:22 +0800

    Update CaseRecord.java

--- src/main/java/com/sicmed/remote/web/entity/CaseRecord.java
+++ src/main/java/com/sicmed/remote/web/entity/CaseRecord.java
@@ -1,5 +1,6 @@
 package com.sicmed.remote.web.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import javax.validation.constraints.Max;
@@ -13,6 +14,7 @@ import javax.validation.constraints.Size;
  * @version 1.0
  */
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CaseRecord extends BaseEntity {
     private String id;
 
