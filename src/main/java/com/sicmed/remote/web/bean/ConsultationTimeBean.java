6d2dd913fd6877df8caa64ed854888dd496fd5e6
MaxCoder <MaxCoderCh@users.noreply.github.com>
2018-12-24 22:20:12 +0800

    添加会诊时间参数bean

---
+++ src/main/java/com/sicmed/remote/web/bean/ConsultationTimeBean.java
@@ -0,0 +1,13 @@
+package com.sicmed.remote.web.bean;
+
+import lombok.Data;
+
+/**
+ * @author MaxCoder
+ */
+@Data
+public class ConsultationTimeBean {
+
+    private String startTime;
+    private String entTime;
+}
