5f2f05dc28c2eab0d0cb19d2db0f83ff15b4cbe7
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2018-12-26 18:07:03 +0800

    会诊详细信息实体类封装

--- src/main/java/com/sicmed/remote/web/bean/ApplyFormBean.java
+++ src/main/java/com/sicmed/remote/web/bean/ApplyFormBean.java
@@ -1,9 +1,13 @@
 package com.sicmed.remote.web.bean;
 
 import com.sicmed.remote.web.entity.ApplyForm;
+import com.sicmed.remote.web.entity.ApplyTime;
+import com.sicmed.remote.web.entity.CaseContent;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -11,4 +15,27 @@ public class ApplyFormBean extends ApplyForm implements Serializable {
 
     List<String> consultationStatusList;
     List<String> consultationTypeList;
-}
+
+
+    List<ApplyTime> applyTimeList;
+    List<CaseContent> caseContentList;
+
+
+    private String patientSex;
+    private Integer patientAge;
+    private Integer patientHeight;
+    private Integer patientWeight;
+    private String caseDiagnosis;
+
+
+    private Date consultantStartTime;
+    private Date consultantEndTime;
+    private String consultantReport;
+    private String consultantFeedback;
+    private BigDecimal consultantPrice;
+    private BigDecimal hospitalPrice;
+
+
+    private String detailAddress;
+
+  }
