7d041d3cb7402eaa1ba6a39aca3fa3c1078b0dd2
MR-L0927 <37697912+MR-L0927@users.noreply.github.com>
2018-12-29 14:40:09 +0800

    医政 受邀会诊  各个点击功能

--- src/main/java/com/sicmed/remote/web/controller/ApplyDisposeController.java
+++ src/main/java/com/sicmed/remote/web/controller/ApplyDisposeController.java
@@ -1,7 +1,6 @@
 package com.sicmed.remote.web.controller;
 
 import com.sicmed.remote.common.ConsultationStatus;
-import com.sicmed.remote.web.YoonaLtUtils.YtDateUtils;
 import com.sicmed.remote.web.entity.ApplyForm;
 import com.sicmed.remote.web.entity.ApplyTime;
 import com.sicmed.remote.web.entity.CaseConsultant;
@@ -54,23 +53,19 @@ public class ApplyDisposeController extends BaseController {
      * 医政 工作台 修改日期
      */
     @PostMapping(value = "sirUpdateDate")
-    public Map sirUpdateDate(String applyFormId, String eventStartTime, String eventEndTime) {
-        if (StringUtils.isBlank(eventStartTime) || StringUtils.isBlank(eventEndTime) || StringUtils.isBlank(applyFormId)) {
-            return badRequestOfArguments("传入参数不全");
+    public Map sirUpdateDate(ApplyTime applyTime) {
+        if (applyTime == null) {
+            return badRequestOfArguments("传入参数有误");
         }
 
-        int j = applyTimeService.delByApplyForm(applyFormId);
+        int j = applyTimeService.delByApplyForm(applyTime.getApplyFormId());
         if (j < 1) {
             return badRequestOfArguments("删除原applyTime失败");
         }
 
         String userId = getRequestToken();
 
-        ApplyTime applyTime = new ApplyTime();
-        applyTime.setApplyFormId(applyFormId);
         applyTime.setUpdateUser(userId);
-        applyTime.setEventStartTime(YtDateUtils.stringToDate(eventStartTime));
-        applyTime.setEventEndTime(YtDateUtils.stringToDate(eventEndTime));
         int k = applyTimeService.updateByForm(applyTime);
         if (k < 1) {
             return badRequestOfArguments("确认时间,time修改失败");
@@ -82,20 +77,35 @@ public class ApplyDisposeController extends BaseController {
     // 医政 工作台 修改价格 数据库暂无表格
 
     /**
-     * 更新申请状态
+     * 医政 更新申请状态
      *
      * @param applyForm
+     * @param applyTime
      * @param applyStatus
      * @param msg1
      * @param msg2
+     * @param report
      */
-    public Map updateStatus(ApplyForm applyForm, ApplyTime applyTime, String applyStatus, String msg1, String msg2) {
+    public Map updateStatus(ApplyForm applyForm, ApplyTime applyTime, String applyStatus, String msg1,
+                            String msg2, String report) {
 
         if (StringUtils.isBlank(applyForm.getId())) {
             return badRequestOfArguments("applyForm.id is null");
         }
 
         String userId = getRequestToken();
+
+        if (StringUtils.isNotBlank(report)) {
+
+            CaseConsultant caseConsultant = new CaseConsultant();
+            caseConsultant.setId(applyForm.getId());
+            caseConsultant.setConsultantReport(report);
+            caseConsultant.setUpdateUser(userId);
+            int i = caseConsultantService.updateByPrimaryKeySelective(caseConsultant);
+            if (i < 1) {
+                return badRequestOfArguments("拒收原因填写失败");
+            }
+        }
 
         int i = applyFormService.updateStatus(applyForm, applyStatus, userId);
         if (i < 1) {
@@ -112,75 +122,59 @@ public class ApplyDisposeController extends BaseController {
         return succeedRequest(applyForm);
     }
 
+
     /**
-     * 医政 受邀会诊 拒收原因
+     * 医政 受邀会诊 拒收
      */
-    public int sirRepulseReport(String applyFormId, String report) {
-        String userId = getRequestToken();
-        CaseConsultant caseConsultant = new CaseConsultant();
-        caseConsultant.setId(applyFormId);
-        caseConsultant.setConsultantReport(report);
-        caseConsultant.setUpdateUser(userId);
-        int i = caseConsultantService.updateByPrimaryKeySelective(caseConsultant);
-        return i;
+    @PostMapping(value = "sirReceiveMasterReject")
+    public Map sirConsultationMasterReject(ApplyForm applyForm, ApplyTime applyTime, String report) {
+
+        String applyStatus = String.valueOf(ConsultationStatus.CONSULTATION_MASTER_REJECT);
+
+        String msg1 = "受邀会诊收诊医政拒收,form修改失败";
+        String msg2 = "受邀会诊收诊医政拒收,time修改失败";
+
+        return updateStatus(applyForm, applyTime, applyStatus, msg1, msg2, report);
     }
 
-
     /**
-     * 医政 受邀会诊 待收诊 接受
+     * 医政 受邀会诊 待收诊 接收
      */
-    @PostMapping(value = "sirConsultationMasterAccede")
-    public Map sirConsultationMasterAccede(ApplyForm applyForm, ApplyTime applyTime) {
+    @PostMapping(value = "sirReceiveMasterAccede")
+    public Map sirConsultationMasterAccede(ApplyForm applyForm, ApplyTime applyTime, String report) {
 
         String applyStatus = String.valueOf(ConsultationStatus.CONSULTATION_MASTER_ACCEDE);
         String msg1 = "受邀会诊收诊医政待收诊接受,form修改失败";
         String msg2 = "受邀会诊收诊医政待收诊接受,time修改失败";
 
-        return updateStatus(applyForm, applyTime, applyStatus, msg1, msg2);
+        return updateStatus(applyForm, applyTime, applyStatus, msg1, msg2, report);
     }
 
     /**
-     * 医政 受邀会诊 待收诊 拒收
+     * 医政 受邀会诊  排期审核 接收
      */
-    @PostMapping(value = "sirConsultationMasterReject")
-    public Map sirConsultationMasterReject(ApplyForm applyForm, ApplyTime applyTime, String report) {
-
-        int i = sirRepulseReport(applyForm.getId(), report);
-        if (i < 1) {
-            return badRequestOfArguments("拒收原因填写失败");
-        }
-
-        String applyStatus = String.valueOf(ConsultationStatus.CONSULTATION_MASTER_REJECT);
-        String msg1 = "受邀会诊收诊医政待收诊拒绝,form修改失败";
-        String msg2 = "受邀会诊收诊医政待收诊拒绝,time修改失败";
-
-        return updateStatus(applyForm, applyTime, applyStatus, msg1, msg2);
-    }
-
-    /**
-     * 医政 受邀会诊  排期审核 接受
-     */
-    @PostMapping(value = "sirDateCheckAccede")
-    public Map sirDateCheckAccede(ApplyForm applyForm, ApplyTime applyTime) {
+    @PostMapping(value = "sirReceiveDateCheckAccede")
+    public Map sirDateCheckAccede(ApplyForm applyForm, ApplyTime applyTime, String report) {
 
         String applyStatus = String.valueOf(ConsultationStatus.CONSULTATION_DATETIME_LOCKED);
         String msg1 = "受邀会诊收诊医政排期审核接受,form修改失败";
         String msg2 = "受邀会诊收诊医政排期审核接受,time修改失败";
 
-        return updateStatus(applyForm, applyTime, applyStatus, msg1, msg2);
+        return updateStatus(applyForm, applyTime, applyStatus, msg1, msg2, report);
     }
 
+
     /**
-     * 医政 受邀会诊  排期审核 拒收
+     * 医政 受邀会诊 砖家协调 确认协调
      */
-    @PostMapping(value = "sirDateCheckReject")
-    public Map sirDateCheckReject(ApplyForm applyForm, ApplyTime applyTime) {
+    @PostMapping(value = "sirReceiveHarmonizeAccede")
+    public Map sirReceiveHarmonizeAccede(ApplyForm applyForm, ApplyTime applyTime, String report) {
 
-        String applyStatus = String.valueOf(ConsultationStatus.CONSULTATION_MASTER_REJECT);
-        String msg1 = "受邀会诊收诊医政排期审核拒绝,form修改失败";
-        String msg2 = "受邀会诊收诊医政排期审核拒绝,time修改失败";
+        String applyStatus = String.valueOf(ConsultationStatus.CONSULTATION_DATETIME_LOCKED);
+        String msg1 = "受邀会诊收诊医政确认砖家协调,form修改失败";
+        String msg2 = "受邀会诊收诊医政确认砖家协调,time修改失败";
 
-        return updateStatus(applyForm, applyTime, applyStatus, msg1, msg2);
+        return updateStatus(applyForm, applyTime, applyStatus, msg1, msg2, report);
     }
     
 }
