27bc0ab4810c348457a3cd2fe569b5e5b3a49a06
MaxCoder <MaxCoderCh@users.noreply.github.com>
2019-01-01 22:38:19 +0800

    添加会诊发送成功详情跳转

--- src/main/resources/public/js/sendSuccess.js
+++ src/main/resources/public/js/sendSuccess.js
@@ -2,12 +2,10 @@ $(function () {
     if(JSON.parse(sessionStorage.getItem('sendOrderData'))){
         var data = JSON.parse(sessionStorage.getItem('sendOrderData'));
         console.log(data);
-        sessionStorage.removeItem('sendOrderData');
-        console.log(data);
 
-        $('.case').html(data.case);
-        $('.recipient').html(data.recipient);
-        $('.sender').html(data.sender);
+        $('.case').html(data.caseSummary);
+        $('.recipient').html(data.inviteSummary);
+        $('.sender').html(data.applySummary);
         $('.details').attr('name', data.id);
     }
     $('.goIndex').click(function () {
@@ -20,9 +18,8 @@ $(function () {
     // 查看此订单按钮 怎么跳到相应的订单页面
 
     $('.details').click(function(){
-        localStorage.setItem('orderId', data.id);
-        localStorage.setItem('orderType', 3);
-        window.location = ' ../page/toAudit.html';
+        sessionStorage.setItem('applyFormId', data.id);
+        window.location = ' ../page/Rfinish.html';
     })
 
 })
