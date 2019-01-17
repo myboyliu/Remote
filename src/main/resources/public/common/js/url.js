96ae76463db43c36183d98876009f1a6f00748f0
MR-L0927 <37697912+MR-L0927@users.noreply.github.com>
2019-01-17 16:21:11 +0800

    Update url.js

--- src/main/resources/public/common/js/url.js
+++ src/main/resources/public/common/js/url.js
@@ -2,9 +2,7 @@
  * 统一接口管理JS
  * @type {string}
  */
-// const baseUrl = "http://192.168.0.222"; //服务地址
-
-const baseUrl = "http://127.0.0.1"; //服务地址
+const baseUrl = "http://192.168.0.222"; //服务地址
 
 const getAllHospital = "/hospital/select"; //查询所有医院类别
 
