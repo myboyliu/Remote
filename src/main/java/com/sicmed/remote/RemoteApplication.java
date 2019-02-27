3ef8e006901fb7a967cf66220c54d41dee7cac8a
663654148@qq.com <30288393+xuebaosong0601@users.noreply.github.com>
2019-02-27 14:52:03 +0800

    移动云起云接口
    移动云起云接口,修改相应配置

--- src/main/java/com/sicmed/remote/RemoteApplication.java
+++ src/main/java/com/sicmed/remote/RemoteApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-import javax.xml.crypto.Data;
 
 @EnableCaching
 @SpringBootApplication
-@MapperScan({"com.sicmed.remote.web.mapper","com.sicmed.remote.tencent.mapper"})
+@MapperScan({"com.sicmed.remote.web.mapper","com.sicmed.remote.tencent.mapper","com.sicmed.remote.yunqiyun.mapper"})
 public class RemoteApplication {
     public static void main(String[] args) {
         SpringApplication.run(RemoteApplication.class, args);
