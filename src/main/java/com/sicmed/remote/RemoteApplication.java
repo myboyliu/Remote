9d2bf1e366c239217ea87c494381037a96d9a1ba
663654148@qq.com <30288393+xuebaosong0601@users.noreply.github.com>
2019-03-05 15:32:00 +0800

    获取springbean的封装

--- src/main/java/com/sicmed/remote/RemoteApplication.java
+++ src/main/java/com/sicmed/remote/RemoteApplication.java
@@ -1,9 +1,11 @@
 package com.sicmed.remote;
 
+import com.sicmed.remote.meeting.util.SpringUtil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
 
 
 @EnableCaching
@@ -11,6 +13,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @MapperScan({"com.sicmed.remote.web.mapper","com.sicmed.remote.tencent.mapper","com.sicmed.remote.yunqiyun.mapper","com.sicmed.remote.live.mapper","com.sicmed.remote.meeting.mapper"})
 public class RemoteApplication {
     public static void main(String[] args) {
-        SpringApplication.run(RemoteApplication.class, args);
+        ApplicationContext app = SpringApplication.run(RemoteApplication.class, args);
+        SpringUtil.setApp(app);
     }
 }
