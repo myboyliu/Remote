23f17a06b5a9a80e5624e0992f0bf53f850ce176
663654148@qq.com <30288393+xuebaosong0601@users.noreply.github.com>
2019-03-05 15:32:29 +0800

    自定义云起云相关参数类

--- src/main/java/com/sicmed/remote/meeting/bean/MeetingBean.java
+++ src/main/java/com/sicmed/remote/meeting/bean/MeetingBean.java
@@ -1,4 +1,36 @@
 package com.sicmed.remote.meeting.bean;
 
-public class MeetingBean {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MeetingBean implements Serializable {
+
+    private String account;             //组会人账号
+
+    private String accountPwd;          //组会人密码（第一次创建会议是才会返回）
+
+    private String appointmentId;       //会议日程id
+
+    private String appointmentName;     //会诊室名称
+
+    private String appointmentNumber;   //会诊室号码
+
+    private double appointmentPeriod;   //会议时长
+
+    private String livePwd;             //直播密码
+
+    private String hostPwd;             //会议密码
+
+    private String liveUrl;             //直播链接
+
+    private Long startTime;           //会诊开始时间
+
+    private String webrtcUrl;           //会议链接
+
+    private String cid;                 //会议id
+
 }
--- src/main/java/com/sicmed/remote/meeting/entity/Meeting.java
+++ src/main/java/com/sicmed/remote/meeting/entity/Meeting.java
@@ -1,4 +1,40 @@
 package com.sicmed.remote.meeting.entity;
 
-public class Meeting {
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Meeting implements Serializable {
+
+    private String mobile;              //手机号
+
+    private String realName;            //真实名
+
+    private String appointmentName;     //会诊室名称
+
+    private String appointmentId;       //会议id
+
+    private String appointmentPeriod;   //会议时长，单位：小时 ，ps: 1.2
+
+    private String appointmentNumber;   //会议室号码
+
+    private String appointmentType;     //会议类型  1：预约会议
+
+    private String hostPwd;             //会议密码
+
+    private boolean isLive;             //是否开启直播
+
+    private boolean isMute;             //参会者进入时是否静音
+
+    private boolean isRecord;           //是否开启录制
+
+    private String livePwd;             //直播密码
+
+    private String startTime;           //会诊开始时间
+
+    private String endTime;             //会诊结束时间(通过它计算时长)
+
+    private int concurrentNum;          //所需并发数
+
 }
