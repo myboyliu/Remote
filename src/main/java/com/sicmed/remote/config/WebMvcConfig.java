6c9435d8f9c1ce64ffe8dcb588293a873568b2d1
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2019-03-05 16:45:18 +0800

    静态 路径跳转

---
+++ src/main/java/com/sicmed/remote/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.sicmed.remote.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+  public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+     registry.addViewController("/dist").setViewName("forward:/dist/index.html");
+     registry.addViewController("/dist/").setViewName("forward:/dist/index.html");
+     registry.addViewController("/app").setViewName("forward:/app/index.html");
+     registry.addViewController("/app/").setViewName("forward:/app/index.html");
+     registry.addViewController("/page").setViewName("forward:/page/login.html");
+     registry.addViewController("/page/").setViewName("forward:/page/login.html");
+      }
+
+
+  } 
