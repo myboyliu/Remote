d983829b16388118aa500caa43009646353fc489
MR-L0927 <37697912+MR-L0927@users.noreply.github.com>
2019-03-13 19:23:55 +0800

    RedisConfiguration配置redis连接

--- src/main/java/com/sicmed/remote/OtherConfiguration/RedisConfiguration.java
+++ src/main/java/com/sicmed/remote/OtherConfiguration/RedisConfiguration.java
@@ -1,18 +1,46 @@
 package com.sicmed.remote.OtherConfiguration;
 
 import com.sicmed.remote.task.RedisTimerListener;
+import org.apache.commons.lang3.StringUtils;
+import org.omg.CORBA.PRIVATE_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.validation.annotation.Validated;
+import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 public class RedisConfiguration {
+
+    @Value("${spring.redis.database}")
+    private int index;
+    @Value("${spring.redis.host}")
+    private String hostName;
+    @Value("${spring.redis.port}")
+    private int port;
+    @Value("${spring.redis.password}")
+    private String password;
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxIdle;
+//    @Value("${jedis.pool.maxTotal}")
+//    private int maxTotal;
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private long maxWaitMillis;
+//    @Value("${jedis.pool.testOnBorrow}")
+//    private boolean testOnBorrow;
+//    @Value("${jedis.pool.testOnReturn}")
+//    private boolean testOnReturn;
+
     @Autowired
     private RedisConnectionFactory redisConnectionFactory;
-
 
     @Bean
     public ChannelTopic expiredTopic() {
@@ -30,4 +58,32 @@ public class RedisConfiguration {
     public RedisTimerListener keyExpiredListener() {
         return new RedisTimerListener(this.redisMessageListenerContainer());
     }
+
+    public RedisConnectionFactory connectionFactory() {
+        JedisConnectionFactory jedis = new JedisConnectionFactory();
+        jedis.setHostName(hostName);
+        jedis.setPort(port);
+        if (StringUtils.isNotBlank(password)) {
+            jedis.setPassword(password);
+        }
+        if (index != 0) {
+            jedis.setDatabase(index);
+        }
+        jedis.setPoolConfig(poolCofig());
+        // 初始化连接pool
+        jedis.afterPropertiesSet();
+        RedisConnectionFactory factory = jedis;
+        return factory;
+    }
+
+    public JedisPoolConfig poolCofig() {
+        JedisPoolConfig poolCofig = new JedisPoolConfig();
+        poolCofig.setMaxIdle(maxIdle);
+//        poolCofig.setMaxTotal(maxTotal);
+        poolCofig.setMaxWaitMillis(maxWaitMillis);
+//        poolCofig.setTestOnBorrow(testOnBorrow);
+//        poolCofig.setTestOnReturn(testOnReturn);
+        return poolCofig;
+    }
+
 }
