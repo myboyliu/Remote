334736983fa6d4fb64b79bd5d7cea07c9bd38c94
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2019-03-13 16:27:43 +0800

    添加 消息 类型 枚举

---
+++ src/main/java/com/sicmed/remote/message/bean/MessageConstant.java
@@ -0,0 +1,24 @@
+package com.sicmed.remote.message.bean;
+
+/**
+ * 消息 类型
+ */
+public enum MessageConstant {
+    //系统 提醒消息
+    SYSTEM_ALERT_MESSAGE,
+    //系统 推送消息
+    SYSTEM_PUSH_MESSAGE,
+    //直播开始 提醒消息
+    LIVE_ALERT_MESSAGE,
+    //新直播   推送消息
+    LIVE_PUSH_MESSAGE,
+    //点播课程开始 提醒消息
+    VIDEO_ALERT_MESSAGE,
+    //新点播课程   推送消息
+    VIDEO_PUSH_MESSAGE,
+    //会议开始 提醒消息
+    MEETING_ALERT_MESSAGE,
+    //会议公开 推送消息
+    MEETING_PUSH_MESSAGE
+
+}
